851c9ec61abf774cb19c43a83abdaea2485d0f73
Patrick O'Keefe <patrick@Patricks-MacBook-Pro.local>
2019-09-23 01:52:40 +0200

    msg

--- benchmark.cpp
+++ benchmark.cpp
@@ -5,9 +5,15 @@ using namespace std;
 
 int main()
 {
-    mt19937 mt_rand(time(0));
+    // mt19937 mt_rand(time(0));
+    // https://stackoverflow.com/questions/15509270/does-stdmt19937-require-warmup
+    std::seed_seq seq{1, 2, 3, 4, 5};
+    mt19937 mt_rand(seq);
 
     cout << mt_rand() << endl;
 
     return 0;
 }
+
+// clang++ -std=c++11 benchmark.cpp  -o benchmark
+// ./benchmark
